f6735a94720e81ea8173d6798d4473029e440be8
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-07 17:03:11 +0200

    Done selectros and WIP step 2 manipulation

--- 05_code-dom_intro_selectors/script.js
+++ 05_code-dom_intro_selectors/script.js
@@ -1,2 +1,30 @@
 "use strict";
 console.log("JS finished loading");
+
+const firstDiv = document.getElementById("first");
+const secondDiv = document.getElementById("second");
+firstDiv.innerHTML = "I have an id of FIRST";
+secondDiv.innerHTML = "I am the second div";
+
+const htmlCollection = document.getElementsByClassName("third");
+
+const elementsArr = Array.from(htmlCollection);
+
+const tagsHtmlCollection = document.getElementsByTagName("div");
+
+const htmlElement = document.querySelector(".third");
+
+const htmlNodesList = document.querySelectorAll(".third, #first");
+
+let isHappy = true;
+
+if (isHappy === true) firstDiv.style.border = "5px solid red";
+
+const myFontsPx = 25;
+
+firstDiv.style.fontSize = `${myFontsPx}px`;
+firstDiv.id = "crazyId";
+
+firstDiv.className = "myOwnClass anotherCrazyClass";
+firstDiv.className += "concatenatedClass";
+console.log(firstDiv);
--- 06_code-dom_manipulation/script.js
+++ 06_code-dom_manipulation/script.js
@@ -1,2 +1,32 @@
 "use strict";
 console.log("JS finished loading");
+
+let textNode = document.createTextNode(
+  "I am a text node and I can only contain text, I cannot contain children"
+);
+
+let pElement = document.createElement("p").appendChild(textNode);
+
+document.body.appendChild(pElement);
+
+// Creation of element
+let h2Element = document.createElement("h2");
+
+h2Element.innerText = "I have just been created!";
+
+//document.body.appendChild(h2Element);
+document.getElementById("content").appendChild(h2Element);
+/* 
+let gLink = document.getElementById("google-link");
+
+gLink.setAttribute("href", "https://www.google.com");
+
+console.log(gLink.getAttribute("href"));
+
+// Short form for production code
+document
+  .getElementById("google-link")
+  .setAttribute("href", "https://www.ironhack.com");
+gLink.removeAttribute("id");
+
+console.log(gLink); */
